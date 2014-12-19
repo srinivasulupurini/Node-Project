2a912d026b437d14d76a3fce5127b71f514624d0
srini <srinivasulu.purini@object-frontier.com>
2014-12-19 18:24:40 +0530

    Added https support

--- FileUpload.js
+++ FileUpload.js
@@ -1,9 +1,16 @@
   var express = require('express'),
 	app = module.exports = express(),
 	handlers = require('./handlers'),
-	routes = require('./routes');
+	routes = require('./routes'),
+	https=require('https'),
+	fs = require('fs');
   
+  
+  var options = {
+  //certificate name SecureNodeFileUpload
+  key: fs.readFileSync('files/0101fc93-27bf-4ae5-9f42-0ba24c5e68c9.private.pem'),
+  cert: fs.readFileSync('files/0101fc93-27bf-4ae5-9f42-0ba24c5e68c9.public.pem')
+};
 //download files
 routes(app,handlers)
-app.listen(7000);
-
+https.createServer(options,app).listen(7000);
